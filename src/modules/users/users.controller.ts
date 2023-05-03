fbad6203c7eddd3a6042fc1f06dbcade01a21287
Kaio Vinicius <69688248+Carnatie@users.noreply.github.com>
2023-05-03 17:19:20 -0300

    feat(users): Add users services
    users CRUD is ready

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Put,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { updateUserDto } from './users.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get(':userId')
+  getUser(@Param('userId', ParseIntPipe) id: number) {
+    return this.usersService.getUser(id);
+  }
+
+  @Get()
+  getUsers() {
+    return this.usersService.getUsers();
+  }
+
+  @Put(':userId')
+  updateUser(
+    @Param('userId', ParseIntPipe) id: number,
+    @Body() data: updateUserDto,
+  ) {
+    return this.usersService.updateUser(id, data);
+  }
+
+  @Delete(':userId')
+  deleteUser(@Param('userId', ParseIntPipe) id: number) {
+    return this.usersService.deleteUser(id);
+  }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,84 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/providers/prisma/prisma.service';
+import { updateUserDto } from './users.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async getUser(id: number) {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+      },
+    });
+    if (!user) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'User not found!',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return user;
+  }
+
+  async getUsers() {
+    const users = await this.prisma.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+      },
+    });
+    return users;
+  }
+
+  async updateUser(id: number, data: updateUserDto) {
+    const user = await this.prisma.user.findFirst({
+      where: { id: id },
+    });
+    if (!user) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'User not found!',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    const updatedUser = await this.prisma.user.update({
+      where: { id },
+      data: data,
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+    return updatedUser;
+  }
+
+  async deleteUser(id) {
+    const user = await this.prisma.user.findFirst({
+      where: { id },
+    });
+    if (!user) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'User not found!',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    await this.prisma.user.deleteMany({ where: { id } });
+    return HttpStatus.ACCEPTED;
+  }
+}
